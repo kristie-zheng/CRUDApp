8288b19eaafa3844b4db7a963ad9cf3f228e0d22
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-01 12:28:23 -0700

    Adjust jQuery function to get the user-entered snippet from the textArea

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
   //$('body').html('<p>HW</p>');
-  $('.submitForm').on('click', function() {
-    let textFieldValue = $('.textField').val();
-    $('.debug').text(textFieldValue);
-  })
+  $('.setData').on('click', function() {
+    //let textFieldValue = $('.textField').val();
+    let snippet = $('.codeSnippet').val();
+    // $('.debug').text(textFieldValue);
+      localStorage.setItem('myFormTextData', snippet);
+  });
+
+$('.getData').on('click', function() {
+  let retrievedData = localStorage.getItem('myFormTextData');
+  $('.snippetDisplay').text(retrievedData);
+});
+  // $('.textField').on('change', function() {
+  //   let textFieldValue = $('.textField').val();
+  //   $('.debug').text(textFieldValue);
+  // })
+
 });
