ea6ba9bbae6affdf9c7279ba48781236fb1eaba4
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-02 01:34:55 -0700

    Make a for-in loop that iterates through localStorage and displays all snippets

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ $(document).ready(function(){
       localStorage.setItem(snippetTitle, snippet);
   });
 
-$('.search').on('click', function() {
-  let retrievedData = localStorage.getItem('myFormTextData');
-  let searchQuery = $('.searchBar').val();
-  let searchResult = localStorage.getItem(searchQuery);
-  if (searchResult !== null) {
-    $('.displaySnippets').text(searchResult);
-  } else {
-    $('.displaySnippets').text('Your query did not match any existing snippets. Please enter a new query.');
-  }
-  
-});
+  $('.search').on('click', function() {
+    //let retrievedData = localStorage.getItem('myFormTextData');
+    let searchQuery = $('.searchBar').val();
+    let searchResult = localStorage.getItem(searchQuery);
+    if (searchResult !== null) {
+      $('.displaySnippets').text(searchResult);
+    } else {
+      $('.displaySnippets').text('Your query did not match any existing snippets. Please enter a new query.');
+    }
+  });
   // $('.textField').on('change', function() {
   //   let textFieldValue = $('.textField').val();
   //   $('.debug').text(textFieldValue);
   // })
+for (var key in localStorage) {
+  var $eachSnip = $("<textarea></textarea>");
+  $eachSnip.html(localStorage[key]);
+  $(".snippetContainer").append($eachSnip);
+}
+
 
 });
+
