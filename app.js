f7ed4931247e70f4370974e8a460160ba518e906
Kristie Desiree <kly5@mail.sfsu.edu>
2018-06-02 01:18:27 -0700

    Modify search function to display error message if search query not found

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-  //$('body').html('<p>HW</p>');
   $('.setData').on('click', function() {
-    //let textFieldValue = $('.textField').val();
     let snippet = $('.codeSnippet').val();
     let snippetTitle = $('.snippetTitle').val();
     // $('.debug').text(textFieldValue);
@@ -12,7 +10,12 @@ $('.search').on('click', function() {
   let retrievedData = localStorage.getItem('myFormTextData');
   let searchQuery = $('.searchBar').val();
   let searchResult = localStorage.getItem(searchQuery);
-  $('.snippetDisplay').text(searchResult);
+  if (searchResult !== null) {
+    $('.displaySnippets').text(searchResult);
+  } else {
+    $('.displaySnippets').text('Your query did not match any existing snippets. Please enter a new query.');
+  }
+  
 });
   // $('.textField').on('change', function() {
   //   let textFieldValue = $('.textField').val();
